63da3578351a2382fb6133464780b6d8bec0543e
Nila Homar <nilahomar@gmail.com>
2022-04-17 14:20:44 +0200

    project data modified

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,48 +1,7 @@
 import React from "react";
 import "./projects.css";
-import PhotoProject from "../../assets/photography.png";
-import foodProject from "../../assets/food-project.png";
-import BurgerProject from "../../assets/burger.png";
-import GridLayout from "../../assets/grid-layout-project.png";
+import data from './project'
 
-const data = [
-  {
-    id: 1,
-    image: PhotoProject,
-    title: "Nila's Photography",
-    content:
-      "A Photography project created with HTML and CSS where I have portrayed my Food and Still photographs. This is one of the first project I have done since I started my development career.",
-    github: "https://github.com/nilahomar/dci-project",
-    demo: "https://nilahomar.github.io/dci-project/",
-  },
-  {
-    id: 2,
-    image: foodProject,
-    title: "Simple Food Menu",
-    content:
-      "A Photography project created with HTML and CSS where I have portrayed my Food and Still photographs. This is one of the first project I have done since I started my development career.",
-    github: "https://github.com/nilahomar/project-food",
-    demo: "https://nilahomar.github.io/project-food/",
-  },
-  {
-    id: 3,
-    image: BurgerProject,
-    title: "Benny's Burger",
-    content:
-      "Benny's Burger restaurant. A project to demonstrate skills in HTML5, CSS3, Flexbox and SCSS. This was very interesting to learn and incorporate Flexbox and SCSS.",
-    github: "https://github.com/nilahomar/project-bennys-burger",
-    demo: "https://nilahomar.github.io/project-bennys-burger/",
-  },
-  {
-    id: 4,
-    image: GridLayout,
-    title: "Grid Layout",
-    content:
-      "An image Gallery created with CSS Grid layout. I learned a lot about Grid with this project.",
-    github: "https://github.com/nilahomar/project-grid-layout",
-    demo: "https://nilahomar.github.io/project-grid-layout",
-  },
-];
 
 function Projects() {
   return (
